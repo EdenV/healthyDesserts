e7d1e65541a64a6115e6dc3052a5be2a8c0250d1
Eden Vaschon <eden@edenvaschon.com>
2013-11-21 14:29:03 -0500

    First file upload

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HealthyDesserts::Application.config.secret_key_base = 'd14a1d77cf419c5f60328b64bd69da90ad97e1a322956a18cd59981dc8119d8589d95ef151eefb8130de6584c80dac5303dfc207d92ed63ddfaf137d1b413650'
